821386136a43f86201b57f56fb0da4928790c76c
PurpleAlbatros <144843763+PurpleAlbatros@users.noreply.github.com>
2025-05-24 02:14:08 +0200

    update

--- App.js
+++ App.js
@@ -3,26 +3,80 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
   SafeAreaView,
   TextInput,
-  TouchableOpacity,
 } from "react-native";
 
-class App extends react.Component() {
+class App extends React.Component {
   state = { mængde: 0, tilbud: 0, resultat: 0 };
+  onAmountChange = (text) => {
+    this.setState({ mængde: text });
+  };
+
+  onDiscountChange = (text) => {
+    this.setState({ tilbud: text });
+  };
+
+  onButtonPress = () =>
+    this.setState({
+      resultat: this.state.mængde * (this.state.tilbud / 100),
+    });
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={{ alignItems: "center", marginTop: 40 }}>
+          <Text style={{ fontSize: 22 }}> Tilbud udregner</Text>
+        </View>
+        <View
+          style={{
+            marginHorizontal: 45,
+            marginTop: 40,
+            marginBottom: 20,
+          }}
+        >
+          <Text style={{ marginBottom: 10 }}>Total</Text>
+          <TextInput style={styles.input} onChangeText={this.onAmountChange} />
+        </View>
+        <View style={{ marginHorizontal: 45 }}>
+          <Text style={{ marginBottom: 10 }}>Tilbud (%)</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={this.onDiscountChange}
+          />
+        </View>
+
+        <View style={{ alignItems: "center" }}>
+          <TouchableOpacity style={styles.button} onPress={this.onButtonPress}>
+            <Text style={{ color: "white" }}>Udregn</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text
+          style={[
+            styles.resultat,
+            { color: this.state.resultat ? "green" : "black" },
+          ]}
+        >
+          mængden efter tilbuddet er:
+        </Text>
+        <Text
+          style={[
+            styles.result,
+            { color: this.state.resultat ? "green" : "black" },
+          ]}
+        >
+          {this.state.resultat ? this.state.mængde - this.state.resultat : null}
+        </Text>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "lightblue",
+    backgroundColor: "purple",
     flex: 1,
   },
   input: {
@@ -34,8 +88,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     fontSize: 20,
   },
-  btn: {
-    backgroundColor: "blue",
+  button: {
+    backgroundColor: "red",
     alignItems: "center",
     height: 80,
     borderRadius: 10,
@@ -47,6 +101,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
     textAlign: "center",
     fontSize: 22,
+    color: "green",
   },
 });
 export default App;
